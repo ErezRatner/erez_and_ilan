98b7a8f1146d3c0cfd8149bdf14abc74068d40d2
Erez Ratner <erezratner1234@gmail.com>
2022-08-24 12:48:22 +0300

    deleted some unimportent files

--- hello.py
+++
@@ -1,9 +0,0 @@
-print("hello")
-
-print("change1")
-
-print("jerd")
-print("change1")
-
-print("fdsfds")
-print("tttttttttttt")
--- passwords.py
+++
@@ -1,8 +0,0 @@
-import json
-
-
-def sign():
-
-    username=input("enter a user name")
-    password=input("enter a password")
-    
